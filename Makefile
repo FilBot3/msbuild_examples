fcb3349141544b936cf03031a0685c57885571a9
Phillip Dudley <Predatorian3@gmail.com>
2015-01-18 20:05:04 -0600

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+msbuild_example_02 : msbuild_example_02.vb
+	vbnc -r:Mirosoft.VisualBasic.dll msbuild_example_02.vb -out:msbuild_example_02.exe
---
+++ msbuild_example_01.cs
@@ -0,0 +1,19 @@
+/*
+
+*/
+
+using System;
+
+class HelloWorld
+{
+	static void Main()
+	{
+		#if DebugConfig
+			Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
+		#endif
+
+		Console.WriteLine("Hello World!");
+	}
+}
+
+
